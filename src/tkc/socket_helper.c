3c1deb7bf97033ee7c20a897fb4da625d0612b92
lixianjing <xianjimli@hotmail.com>
2022-04-23 14:57:08 +0800

    improve socket_resolve

--- src/tkc/socket_helper.c
+++ src/tkc/socket_helper.c
@@ -157,6 +157,8 @@ struct sockaddr* socket_resolve(const char* host, int port, struct sockaddr_in* 
   memset(addr, 0x00, sizeof(*addr));
   addr->sin_family = AF_INET;
   addr->sin_port = htons(port);
+  return_value_if_fail(h->h_addrtype == AF_INET, NULL);
+
   memcpy(&(addr->sin_addr.s_addr), h->h_addr, h->h_length);
 
   return (struct sockaddr*)addr;
