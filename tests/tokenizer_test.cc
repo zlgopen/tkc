ec5eaf111be986ea15e1b90029d0c30db081fddf
lixianjing <xianjimli@hotmail.com>
2022-05-26 07:00:12 +0800

    improve tokenizer/func_call_parser to support quoted string

--- tests/tokenizer_test.cc
+++ tests/tokenizer_test.cc
@@ -203,7 +203,7 @@ TEST(Tokenizer, str) {
   ASSERT_STREQ(tokenizer_next_str(t), "e=123");
   
   ASSERT_STREQ(tokenizer_next(t), "d");
-  ASSERT_STREQ(tokenizer_next(t), "123");
+  ASSERT_STREQ(tokenizer_next(t), "'123");
 
   tokenizer_deinit(t);
 }
