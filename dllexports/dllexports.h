154f571355f1282c508c06a88cfed01ad1b8f105
lixianjing <xianjimli@hotmail.com>
2022-08-31 10:03:10 +0800

    export some funcs for awtk

---
+++ dllexports/dllexports.h
@@ -0,0 +1,20 @@
+/**
+ * @method mz_zip_reader_init
+ * @annotation ["global"]
+ */
+/**
+ * @method mz_zip_reader_init_mem
+ * @annotation ["global"]
+ */
+/**
+ * @method mz_zip_reader_locate_file
+ * @annotation ["global"]
+ */
+/**
+ * @method mz_zip_reader_extract_to_heap
+ * @annotation ["global"]
+ */
+/**
+ * @method mz_zip_end
+ * @annotation ["global"]
+ */
