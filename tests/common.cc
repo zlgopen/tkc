3c79d92e773202a1d4d1c8cfd1133d6cd0f22fdf
lixianjing <xianjimli@hotmail.com>
2025-05-16 17:36:10 +0800

    improve str_append_wchar_with_len

---
+++ tests/common.cc
@@ -0,0 +1,17 @@
+﻿#include "common.h"
+#include "tkc/utf8.h"
+#include "gtest/gtest.h"
+
+#define PRINTF printf
+
+#include <string>
+
+using std::string;
+
+void assert_str_eq(const wchar_t* wstr, const char* utf8) {
+  char str[256];
+
+  tk_utf8_from_utf16(wstr, str, sizeof(str));
+  ASSERT_EQ(string(str), string(utf8));
+}
+
---
+++ tests/common.h
@@ -0,0 +1,8 @@
+#include "tkc/rect.h"
+
+#ifndef COMMON_H
+#define COMMON_H
+
+void assert_str_eq(const wchar_t* wstr, const char* utf8);
+
+#endif /*COMMON_H*/
