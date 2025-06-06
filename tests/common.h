d5ee1139f05f484cc6dc89bc3ecc3be8bb2d20f4
lixianjing <xianjimli@hotmail.com>
2025-06-06 17:35:07 +0800

    improve tests

--- tests/common.h
+++ tests/common.h
@@ -1,8 +1,16 @@
-#include "tkc/rect.h"
+#include "tkc/utf8.h"
+#include <string>
+
+using std::string;
 
 #ifndef COMMON_H
 #define COMMON_H
 
-void assert_str_eq(const wchar_t* wstr, const char* utf8);
+static inline void assert_str_eq(const wchar_t* wstr, const char* utf8) {
+  char str[256];
+
+  tk_utf8_from_utf16(wstr, str, sizeof(str));
+  ASSERT_EQ(string(str), string(utf8));
+}
 
 #endif /*COMMON_H*/
--- tests/main.cc
+++ tests/main.cc
@@ -1,64 +1,42 @@
-﻿// Copyright 2006, Google Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//     * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN 1
 #endif /*WIN32_LEAN_AND_MEAN*/
 
 #include <stdio.h>
 
-#include "awtk.h"
-#include "tkc/mem.h"
-#include "base/idle.h"
-#include "base/system_info.h"
-#include "ext_widgets/ext_widgets.h"
+#include "tkc.h"
 #include "gtest/gtest.h"
-#include "demos/assets.h"
-#include "tkc/socket_helper.h"
+#include "fscript_ext/fscript_ext.h"
+#include "conf_io/app_conf.h"
+#include "conf_io/app_conf_init_json.h"
+#include "conf_io/app_conf_init_ubjson.h"
+#include "conf_io/app_conf_init_ini.h"
 
 GTEST_API_ int main(int argc, char** argv) {
   printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
 
-  return_value_if_fail(tk_pre_init() == RET_OK, RET_FAIL);
+  return_value_if_fail(platform_prepare() == RET_OK, RET_FAIL);
   tk_socket_init();
-  system_info_init(APP_SIMULATOR, NULL, NULL);
-  tk_init_internal();
+  data_writer_factory_set(data_writer_factory_create());
+  data_reader_factory_set(data_reader_factory_create());
+  data_writer_factory_register(data_writer_factory(), "file", data_writer_file_create);
+  data_reader_factory_register(data_reader_factory(), "file", data_reader_file_create);
+  data_reader_factory_register(data_reader_factory(), "mem", data_reader_mem_create);
+  data_writer_factory_register(data_writer_factory(), "wbuffer", data_writer_wbuffer_create);
 
-  assets_init();
-  tk_init_assets();
-  tk_ext_widgets_init();
-  widget_resize(window_manager(), 1024, 800);
+  fscript_global_init();
+  fscript_ext_init();
+
   int ret = RUN_ALL_TESTS();
 
-  tk_deinit_internal();
   tk_socket_deinit();
+  app_conf_deinit();
+  fscript_global_deinit();
+  data_writer_factory_destroy(data_writer_factory());
+  data_reader_factory_destroy(data_reader_factory());
+  data_writer_factory_set(NULL);
+  data_reader_factory_set(NULL);
 
   return ret;
 }
